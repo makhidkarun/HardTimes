a64829b0de466e60a36ed6fe4bfeab40cdfffecb
Robert Eaglestone <robert.eaglestone@gmail.com>
2023-08-02 18:36:44 -0500

    Task: added stub for step 1b, cleaned up 1a a bit

--- hard_times/hard_times_lib.js
+++ hard_times/hard_times_lib.js
@@ -28,15 +28,6 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-// STAGE 1. 
-// A. **Biosphere**.  Roll 2D; DMs = +1 if war zone.  +2 if intense war zone.  +3 if black war zone.  +1 if Starport A.  +1 if Population 9+.
-
-// roll < 6 = no effect.
-// 6-8: atmosphere turns _tainted_.
-// 9-10: atmosphere turns _tainted_; pop - 1.  Apply TL-3 in **STAGE 3**.
-// 11-12: atmosphere turns downright bad (but I can't quite read the text).
-// 13+ Dieback.  Atmosphere is now C.
-
 function test_hard_times() {
 
 	var starport = 'A',
@@ -66,14 +57,28 @@ function _test_hard_times( starport, siz, atm, hyd, pop, gov, law, tl, populatio
 	console.log( '   pop mult: ' + population_multiplier );
 	console.log( '   war zone: ' + warZoneLevel );
 
-	var out = doHardTimesStage1( starport, atm, pop, gov, law, population_multiplier, warZoneLevel );
+	var out = doHardTimesStage1a( starport, atm, pop, gov, law, population_multiplier, warZoneLevel );
 
 	console.log( 'output object: ' );
 	console.log( out );
 }
 
-
-function doHardTimesStage1( 
+///////////////////////////////////////////////////////////////////////////////
+//
+// STAGE 1a. 
+//
+// A. **Biosphere**.  Roll 2D.   DMs:
+//      +1 if war zone.  +2 if intense war zone.  +3 if black war zone. 
+//      +1 if Starport A.  +1 if Population 9+.
+//
+// roll < 6 = no effect.
+// 6-8: atmosphere turns _tainted_.
+// 9-10: atmosphere turns _tainted_; pop - 1.  Apply TL-3 in **STAGE 3**.
+// 11-12: atmosphere turns downright bad (but I can't quite read the text).
+// 13+ Dieback.  Atmosphere is now C.
+//
+///////////////////////////////////////////////////////////////////////////////
+function doHardTimesStage1a( 
       starport, a, p, g, l, 
       population_multiplier, 
       warZoneLevel ) {
@@ -112,10 +117,92 @@ function doHardTimesStage1(
   if (p < 0) p = 0;
 
   // build and return response object
-   var uwp = { 
+   var resp = { 
       'starport': starport, 'atm': a, 'pop': p, 'gov': g, 'law': l, 
       'population_multiplier': population_multiplier, 
-      'warZoneLevel': warZoneLevel, 'stage3TLDM': stage3TLDM };
+      'stage3TLDM': stage3TLDM };
 
-   return uwp;
+   return resp;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Stage 1b.
+//
+// B. **Starport**.  Roll 1D.   DMs:
+//      A-class starports:
+//          +2 if frontier === 'Frontier'
+//          +3 if frontier === 'Outlands'
+//          +3 if frontier === 'Wilds'
+//          +warZoneLevel
+//          +2 if isIsolated === true
+//          +1 if pop < 5
+//          +1 if pop < 3 (cumulative)
+//      	11 - TL (max +8)
+//
+//      B-class starports:
+//          +2 if frontier === 'Outlands'
+//          +3 if frontier === 'Wilds'
+//          +warZoneLevel (max +2)
+//          +3 if isIsolated === true
+//          +1 if pop < 5
+//          +1 if pop < 3 (cumulative)
+//			9 - TL (max +7)
+//
+//      C-class starports:
+//          +1 if frontier === 'Outlands'
+//          +2 if frontier === 'Wilds'
+//          +warZoneLevel (adjust by -1 if > 1)
+//          +4 if isIsolated === true
+//          +1 if pop < 3
+//			8 - TL if tl < 7 (max +5)
+//
+//      D-class starports:
+//          +1 if frontier === 'Wilds'
+//          +1 if warZoneLevel === 3
+//          +1 if isIsolated === true
+//          7 - TL if tl < 7 (max +3)
+//
+//   Roll is mapped to Degrees of Change table, which reduces the starport class.
+//
+//   If starport class falls two or more levels, all bases are eliminated.
+//   Otherwise:
+//      7+ on 2D eliminates the naval base
+//      8+ on 2D eliminates the scout base
+//
+//   DMs: 
+//		Frontier +3
+//		Outlands +5
+//		Wilds: base always eliminated if in a War Zone.
+//		War Zone +1
+//		Intense War Zone +2
+//		Black War Zone   +2
+//
+///////////////////////////////////////////////////////////////////////////////
+function doHardTimesStage1b(
+	starport,
+	hasNavalBase,
+	hasScoutBase,
+	frontierStatus,
+	warZoneLevel,
+	isIsolatedWorld,
+	pop,
+	tl
+) {
+	if (warZoneLevel < 0) warZoneLevel = 0;
+    if (warZoneLevel > 3) warZoneLevel = 3;
+
+	///////////////////////////////
+	//
+	//  TODO -- ALL THE INNARDS
+	//
+	///////////////////////////////
+		
+	var resp = {
+		'starport': starport,
+		'hasNavalBase': hasNavalBase,
+		'hasScoutBase': hasScoutBase
+	};
+
+	return resp;
 }
